1d3b95a3cda68cb2b39bd6ee35d431faf9e986ad
Eel2000 <eliezerbwana@gmaill.com>
2020-10-06 16:35:26 +0200

    Add Custom Frame

---
+++ Bootique/Bootique.Android/CustomFrameAndroid.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+namespace Bootique.Droid
+{
+    public class CustomFrameAndroid : FrameRenderer
+    {
+        public CustomFrameAndroid(Context context) : base(context)
+        {
+
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Frame> e)
+        {
+            base.OnElementChanged(e);
+
+            if (e.NewElement == null) return;
+
+            if (e.NewElement.HasShadow)
+            {
+                Elevation = 30.0f;
+                TranslationZ = 0.0f;
+                SetZ(30);
+            }
+        }
+    }
+}
